324409979cbc6cdb4269e819cfa5059e9222b68e
Abhishek Kumar Shaw <avitech2001@gmail.com>
2022-11-30 22:49:36 +0530

    Created detail page for ordering item with increment and decrement

---
+++ app/src/main/java/com/example/babakadhaba/DetailActivity.java
@@ -0,0 +1,15 @@
+
+package com.example.babakadhaba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
