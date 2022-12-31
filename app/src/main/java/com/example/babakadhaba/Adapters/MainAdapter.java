f67930cbd8b7d7acb08d14723de696e360b48ccd
Abhishek Kumar Shaw <avitech2001@gmail.com>
2022-12-31 11:10:54 +0530

    created database in sqlite using code first approach

--- app/src/main/java/com/example/babakadhaba/Adapters/MainAdapter.java
+++ app/src/main/java/com/example/babakadhaba/Adapters/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.example.babakadhaba.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.babakadhaba.DetailActivity;
 import com.example.babakadhaba.Models.MainModel;
 import com.example.babakadhaba.R;
 
@@ -39,6 +41,18 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.viewholder> {
         holder.foodName.setText(model.getName());
         holder.foodPrice.setText(model.getPrice());
         holder.foodDesc.setText(model.getDescription());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("image",model.getImage());
+                intent.putExtra("price",model.getPrice());
+                intent.putExtra("desc",model.getDescription());
+                intent.putExtra("name",model.getName());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/babakadhaba/DetailActivity.java
+++ app/src/main/java/com/example/babakadhaba/DetailActivity.java
@@ -4,12 +4,52 @@ package com.example.babakadhaba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.babakadhaba.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
+
+    ActivityDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final int image =getIntent().getIntExtra("image",0);
+        final int price = Integer.parseInt(getIntent().getStringExtra("price"));
+        String name = getIntent().getStringExtra("name");
+        String description = getIntent().getStringExtra("desc");
+
+        binding.detailImg.setImageResource(image);
+        binding.orderPrice.setText(String.format("%d",price));
+        binding.detailFoodName.setText(name);
+        binding.detailDesc.setText(description);
+
+        final DBHelper helper = new DBHelper(this);
+
+        binding.insertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean isInserted = helper.insertOrder(
+                        binding.nameBox.getText().toString(),
+                        binding.phoneBox.getText().toString(),
+                        price,
+                        image,
+                        name,
+                        description,
+                        Integer.parseInt(binding.quantity.getText().toString())
+                );
+                if(isInserted)
+                    Toast.makeText(DetailActivity.this, "Data Success", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(DetailActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
